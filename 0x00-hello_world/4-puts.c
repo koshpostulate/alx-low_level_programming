7630b83d57af17f84669970a5cc6c9981905efdf
koshpostulate <coshpostulate@gmail.com>
2022-09-08 14:57:09 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main - Serves as the entry point for the program
- *
- * Return: By defult returns zero if no errors otherwise returns non-zero valullue
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
