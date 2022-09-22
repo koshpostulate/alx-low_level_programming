2a179b5148d121a5f06d54e5faccef8b420416bc
koshpostulate <coshpostulate@gmail.com>
2022-09-22 15:13:53 -0700

    tk7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 /**
  * leet - encodes a string into 1337
  * @s: input string.
- * Return: the pointer dest.
+ * Return: s
  */
 
 char *leet(char *s)
@@ -11,6 +11,8 @@ int count = 0, i;
 int lower_case[] = {97, 101, 111, 116, 108};
 int upper_case[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
+
+
 
 while (*(s + count) != '\0')
 {
@@ -22,7 +24,7 @@ if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 break;
 }
 }
-counter++;
+count++;
 }
 
 return (s);
