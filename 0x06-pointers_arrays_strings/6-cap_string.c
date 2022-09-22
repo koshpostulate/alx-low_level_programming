77d4976b025315255554e84be647852404fc52b3
koshpostulate <coshpostulate@gmail.com>
2022-09-22 15:18:24 -0700

    go

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 if (*(s + count) >= 97 && *(s + count) <= 122)
 *(s + count) = *(s + count) - 32;
-counter++;
+count++;
 while (*(s + count) != '\0')
 {
 for (i = 0; i < 13; i++)
